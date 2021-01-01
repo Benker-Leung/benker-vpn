640e52d3a1b6fce876547c0a832264d7807ac5d0
benker <lyleungad@connect.ust.hk>
2021-01-01 14:14:36 +0800

    added replace port & ip helper function

--- lib/replace_header.c
+++ lib/replace_header.c
@@ -1,8 +1,44 @@
 #include "replace_header.h"
 
-// TODO:
-// void replace_src_port(uint8_t *buffer, uint16_t port);
-// void replace_src_ip(uint8_t *buffer, uint32_t ip);
+int validate_ip_tcp_len(uint8_t *buffer, int buffer_size) {
+    // ensure size at least fit ip header + tcp hedaer
+    if (sizeof(struct iphdr) + sizeof(struct tcphdr) > buffer_size) {
+        return 0;
+    }
+    struct iphdr *ip_header = (struct iphdr*)buffer;
+    int ip_header_len = ip_header->ihl*4;
+    // ensure size at least fit ip header + tcp hedaer
+    if (ip_header_len + sizeof(struct tcphdr) > buffer_size) {
+        return 0;
+    }
+    // check total len
+    if (ntohs(ip_header->tot_len) > buffer_size) {
+        return 0;
+    }
+    struct tcphdr *tcp_header = (struct tcphdr*)(buffer + ip_header_len);
+    // check overall length
+    if (ntohs(ip_header->tot_len) < ip_header_len + tcp_header->doff*4) {
+        return 0;
+    }
+    return 1;
+}
+
+void replace_src_port(uint8_t *buffer, uint16_t port) {
+    struct iphdr* ip_header = (struct iphdr*)buffer;
+    int ip_header_size = ip_header->ihl*4;
+    struct tcphdr* tcp_header = (struct tcphdr*)(buffer + ip_header_size);
+    uint8_t* tcp_data = buffer + ip_header_size + tcp_header->doff*4;     // offset ip & tcp header
+    tcp_header->source = htons(port);
+    tcp_header->check = 0;
+    tcp_header->check = tcp_csum(ip_header, tcp_header, tcp_data);
+}
+
+void replace_src_ip(uint8_t *buffer, uint32_t src_ip) {
+    struct iphdr* ip_header = (struct iphdr*)buffer;
+    ip_header->check = 0;   // clear the check-sum
+    ip_header->saddr = htonl(src_ip);
+    ip_header->check = htons(ip_csum(ip_header));
+}
 
 // not supposed to be called by outside
 uint16_t summing (uint8_t *buffer, int bytes, uint32_t sum)
--- lib/replace_header.h
+++ lib/replace_header.h
@@ -6,19 +6,33 @@
 #include <linux/tcp.h>
 
 /**
+ * validate the ip header len, tcp header len, tcp data len (checksum validation is NOT done)
+ * 
+ * @param buffer the buffer pointing to the first byte of the ip header
+ * @param buffer_size the size of buffer in bytes
+ * 
+ * @return 1 if it is valid, else 0
+*/
+int validate_ip_tcp_len(uint8_t *buffer, int buffer_size);
+
+/**
  * replace the source port with given port, and the tcp checksum will be recalculated
+ * buffer assume to be correct in length inside the header fields (not responsible for checking the len etc)
  * 
  * @param buffer the buffer should start with and contains ip header, tcp header, tcp payload
  * @param the port to be put into, e.g "80" means port 80
+ * 
  * 
 */
 void replace_src_port(uint8_t *buffer, uint16_t port);
 
 /**
  * replace the source ip addr with given new ip addr, and the ip checksum will be recalculated
+ * buffer assume to be correct in length inside the header fields (not responsible for checking the len etc)
  * 
  * @param buffer the buffer should start with ip header
  * @param the ip int number, e.g 1 means ip 0.0.0.1
+ * 
  * 
 */
 void replace_src_ip(uint8_t *buffer, uint32_t ip);
